06e4392594877f9296ca1a0a46f230b5d0ae6b47
Imre Bartis <imre.bartis@siili.com>
2018-01-22 10:56:04 +0200

    global namespace

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
-// works in the node REPL:
-fs.readFile('zork.txt', 'utf8', function(err,data){
-    console.log(data)
-})
+var globalFoo
 
-//OR 
-fs.readFile('zork.txt', function(err,data){
-    console.log(data.toString())
-})
+exports.setFoo = function(val) {
+    globalFoo = val
+}
 
-// => Lorem ipsum dolor amet health goth cliche craft beer, blog cloud bread air plant slow-carb mumblecore heirloom mixtape. Austin marfa readymade street art affogato. Cred wayfarers authentic, tote bag cloud bread cornhole meggings tumblr pitchfork. Street art portland church-key, snackwave semiotics seitan literally twee chambray farm-to-table heirloom plaid tousled. YOLO master cleanse bicycle rights fingerstache twee pok pok cronut. Health goth hella green juice enamel pin. Synth offal pug, woke ramps brunch keytar banjo shoreditch.
+exports.returnFoo = function() {
+    return globalFoo
+}
 
-// specifying the length of buffer:
-buf = new Buffer(5)
-// => <Buffer 00 00 00 00 00>
-buf.write('hello world')
-//=> 5
-buf.toString()
-// => 'hello'
+// node REPL:
+// var modFoo = require('./index.js')
+// modFoo.setFoo(42)
+// modFoo.returnFoo()
+// => 42
 
-// using offset:
-buf.write('hello', 2)
-// => 3
-buf.toString()
-// => 'hehel'
-
-//specifying offset (2) + length(1):
-buf.write('xxxx', 2, 1)
-// => 1
-buf.toString()
-// => 'hexel'
-
-buf.write('yyyy', 2, 1, 'utf8')
-// => 1
-buf.toString()
-// 'heyel'
+// global
+// => we can see our mod Foo module that we imported is available, 
+// but globalFoo is not, because GlobalFoo
+//  was global to this code,
+// not global to the application where we were using it.
