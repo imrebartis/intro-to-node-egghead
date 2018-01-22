76b5bc74f852cb8b2fdcab429aaae364ae4f5479
Imre Bartis <imre.bartis@siili.com>
2018-01-22 13:44:46 +0200

    manage memory & garbage collection

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-// node REPL:
-var sayHello = function() { console.log('Hello to you!') }
-var sayGoodbye = function() { console.log('Goodbye then!') }
+var http = require('http')
+var heapdump = require('heapdump')
 
-var EventEmitter = require('events').EventEmitter
-emitter = new EventEmitter()
-emitter.on('hello', sayHello)
-emitter.on('goodbye', sayGoodbye)
-emitter.emit('hello') // => Hello to you!
-emitter.emit('goodbye') // => Goodbye then!
-global.emitter._events // => { hello: [Function: sayHello], goodbye: [Function: sayGoodbye] }
-global.emitter._events.hello.toString() // => 'function () { console.log(\'Hello to you!\') }'
-emitter.on('hello', sayHello)
-emitter.on('hello', sayHello)
-global.emitter._events // =>
-// { hello: 
-//     [ [Function: sayHello],
-//       [Function: sayHello],
-//       [Function: sayHello] ],
-//    goodbye: [Function: sayGoodbye] }
-emitter.removeListener('hello', sayHello) // removes one of the sayHello functions
-emitter.removeAllListeners('hello') // removes all the sayHello functions
-emiter.setMaxListeners(0) // setting the max nr of listeners to unlimited
+
+function BigData(){
+    var mem = Array(1000000).join("a")
+}
+
+var leak = []
+
+var server = http.createServer(function(request, response){
+    if(request.url === "/"){
+        // leak.push(new BigData)
+        response.writeHead(200, {"Content-Type": "text/html"})
+        response.end('Hello World')
+    } 
+})
+
+server.listen(3000)
